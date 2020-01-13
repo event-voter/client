9ccdd9f4762fd368a785f0cd65b2e92ae2855234
nandyo <nandyo6969@gmail.com>
2020-01-13 13:55:04 +0700

    Added more route and more component

---
+++ src/views/NewroomForm.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <div class="label-main margin-greater" align="center">
+        <div class="text-header text-primary margin-greater">New Event</div>
+    </div>
+
+    <div class="margin-greater" align="center">
+        <div class="text-body margin-lower">Nama event</div>
+        <div class="margin-greater-down" style="display:flex;align-content:center;justify-content:center;">
+            <div class="label-inputicon"><i class="fa fa-arrow-right"></i></div><input class="input-basic input-standart" type="text">
+        </div>
+
+        <div class="margin-greater-down" style="display:flex;align-content:center;justify-content:center;">
+            <div class="label-inputicon"><i class="fa fa-arrow-right"></i></div><input class="input-basic input-standart" type="text">
+        </div>
+
+        <div class="button-squareinputencenterer button-secondary margin-greater" v-ripple>Create</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NewRoom'
+}
+</script>
