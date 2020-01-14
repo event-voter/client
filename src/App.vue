b914258c78e5cd87155651e477e92178225595ac
ardhi <ardhi.rofi@gmail.com>
2020-01-14 20:34:41 +0700

    membuat komponen date-box

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,18 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     
     <div class="header-bar"><div class="header-logo"></div></div>
-    <Home msg="a"/>
-    <date-box day="senin" date="1-1-2020"></date-box>
-    <date-box day="senin" date="2-1-2020"></date-box>
-    <date-box day="senin" date="3-1-2020"></date-box>
-    <date-box day="senin" date="4-1-2020"></date-box>
   <router-view></router-view>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Home from '@/views/Homehome.vue'
-import dateBox from "@/components/date-box.vue"
+// import Home from '@/views/Homehome.vue'
+// import dateBox from "@/components/date-box.vue"
 
 export default {
   name: 'App',
   components: {
-    Home,
-    'date-box': dateBox 
   }
 }
 </script>
